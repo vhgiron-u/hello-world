8aef15809d1a10b6e16e535c4c140719379863fa
alulab14 <alulab14@inf.pucp.edu.pe>
2018-05-30 18:48:28 -0500

    ups

---
+++ PanaeriaDiario/Program.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanaeriaDiario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "server=quilla.lab.inf.pucp.edu.pe;user=inf282g7;database=inf282g7;port=3306;password=0mvK88;SslMode=none;";
+            MySqlConnection conexion = new MySqlConnection(cadena);
+            conexion.Open();
+            MySqlCommand comando = new MySqlCommand();
+            comando.Connection = conexion;
+            comando.CommandText = "PEDIDO_DIARIO";
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+}
